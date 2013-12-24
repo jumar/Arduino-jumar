21e3bbe5c992a7de17a4949303e8820fc8017a02
Julien Marbach <julien.marbach@gmail.com>
2013-12-23 21:44:55 -0500

    Added new "white light" mode to use the strand for regular lighting Added serial commands to switch lighting modes and to enable/disable PIR sensor, sound sensor and sleep mode.

--- LightShow/LightShow.ino
+++ LightShow/LightShow.ino
@@ -7,6 +7,8 @@
 #include "TimerOne.h"
 #include <Adafruit_NeoPixel.h>
 
+#define SERIAL_TRACE
+
 //SND is connected on pin 2 (external interrupt 0)
 //and the LED will be flipped when the interrupt is raised
 //SND will go LOW when sound is detected
@@ -23,71 +25,180 @@
 #define STRAND_PIN 10
 
 //timeout in ms before going in sleep mode
-#define NO_MVT_SLEEP_DELAY 15000
+#define NO_MVT_SLEEP_DELAY 60000
 //timeout in ms before shuting down
-#define NO_MVT_SHUTDOWN_DELAY 30000
+#define NO_MVT_SHUTDOWN_DELAY 120000
+//time in ms between two snd events
+#define SND_DELAY 500 
 
 //brightness of strand during light show
 #define HI_BRIGHTNESS 255
 //brightness of strand during standby mode
 #define LO_BRIGHTNESS 16
 
+//number of lighting schemes
 #define NB_SHOW 6
 
 volatile int sndState = LOW; 
 volatile int PIRState = LOW;
 volatile int PIRVal = 0;
 volatile unsigned long lastPIRTime = 0;
-volatile int sndCounter = 0;
+volatile unsigned long lastSNDTime = 0;
 volatile int showType = 0;
 volatile boolean _break = false;
 
 #define SHUTDOWN 0
 #define SLEEP 1
 #define LIGHTSHOW 2
+#define WHITE_LIGHT 3
+#define IDLE_ 4
 
-int state = SHUTDOWN;
+int state = LIGHTSHOW;
+int lastState = state;
 int ledState = LOW;
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(149, STRAND_PIN, NEO_GRB + NEO_KHZ800);
 
+// defines for serial commands
+#define NO_ACTION 245
+#define SND_OFF 'a'
+#define SND_ON 'b'
+#define MVT_OFF 'c'
+#define MVT_ON 'd'
+#define NO_SLEEP 'e'
+#define ALLOW_SLEEP 'f'
+#define WHITE_LIGHT_ON 'g'
+#define LIGHTSHOW_CMD 'h'
+#define SHUTDOWN_CMD 'i'
+byte fromSerial = NO_ACTION;
+
 void setup()
 {
   pinMode(LED, OUTPUT);
-  attachInterrupt(0, sound, FALLING);
-  attachInterrupt(1, movement, CHANGE);
+  lastPIRTime = lastSNDTime = millis();
+  enable_SND(true);
+  enable_MVT(true);
   Timer1.initialize(1000000); // Set period to 1s
   Timer1.attachInterrupt(timerCallback);  // attaches timerCallback() as a timer overflow interrupt
-  //lastPIRTime = millis();
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
   Serial.begin(9600);
-  Serial.println("Go");
+  #ifdef SERIAL_TRACE
+    Serial.println("Go");
+  #endif
+}
+
+void enable_SND(boolean activate)
+{
+  if(activate)
+    attachInterrupt(0, sound, FALLING);
+  else
+    detachInterrupt(0);
+}
+
+void enable_MVT(boolean activate)
+{
+  if(activate)
+    attachInterrupt(1, movement, CHANGE);
+  else
+    detachInterrupt(1);
+}
+
+void enableSleepTimer(boolean enable)
+{
+  if(enable)
+    Timer1.restart();
+  else
+    Timer1.stop();
+}
+
+void serialEvent()
+{
+  fromSerial = Serial.read();
+  #ifdef SERIAL_TRACE
+    Serial.print("Rcv:");
+    Serial.println(fromSerial);
+  #endif
+  switch(fromSerial)
+  {
+    case SND_OFF:
+      enable_SND(false);
+    break;
+    case SND_ON:
+      enable_SND(true);
+    break;
+    case MVT_OFF:
+      enable_MVT(false);
+    break;
+    case MVT_ON:
+      enable_MVT(true);
+    break;
+    case NO_SLEEP:
+      enable_Sleep(false);
+    break;
+    case ALLOW_SLEEP:
+      enable_Sleep(true);
+    break;
+    case WHITE_LIGHT_ON:
+      setState(WHITE_LIGHT);
+    break;
+    case SHUTDOWN_CMD:
+      setState(SHUTDOWN);
+    break;
+    case LIGHTSHOW_CMD:
+      setState(LIGHTSHOW);
+    break;
+  }
+}
+
+void enable_Sleep(boolean enable)
+{
+  if(enable)
+  {
+   lastPIRTime = millis();
+  }
+  if(!enable && (state == SLEEP || state == SHUTDOWN))
+  {
+    setState(lastState);
+  }
+  enableSleepTimer(enable);
+}
+
+void setState(int newState)
+{
+  if(newState != state)
+  {
+    if(state = SLEEP)
+      digitalWrite(LED, LOW); // turn off blinking LED
+    if(state == LIGHTSHOW || state == WHITE_LIGHT)
+      lastState = state;
+    state = newState;
+    _break = true;
+    #ifdef SERIAL_TRACE
+      Serial.print("new State: ");
+      Serial.println(newState);
+    #endif
+  }
 }
 
 void loop()
 {
-  // check for sound
-  if(sndCounter != 0)
-  {
-    //We had sound -> change show type
-    showType++;
-    sndCounter = 0;
-  }
   switch(state)
   {
     default:
     case SHUTDOWN:
       setColor(strip.Color(0, 0, 0));
+       //turn the LED off while sleeping
+      ledState = !ledState;
       digitalWrite(LED, LOW);
     break;
     case SLEEP:
       strip.setBrightness(LO_BRIGHTNESS);
       strip.show();
-      //blink the LED
+      //blink the LED while sleeping
       ledState = !ledState;
       digitalWrite(LED, ledState);
-      delay(800);
+      delay(500);
     break;
     case LIGHTSHOW:
       strip.setBrightness(HI_BRIGHTNESS);
@@ -128,8 +239,16 @@ void loop()
           break;
       }
     break;
+    case WHITE_LIGHT:
+      strip.setBrightness(HI_BRIGHTNESS);
+      colorFull(strip.Color(254, 254, 254), 0);
+      strip.show();
+      setState(IDLE_);
+    break;
+    case IDLE_:
+    // do nothing!
+    break;
   }
-  
   //STATE MACHINE
 }
 
@@ -138,18 +257,20 @@ void movement()
   PIRVal = digitalRead(PIR_PIN); 
   if (PIRVal == HIGH) 
   { 
-    if (PIRState == LOW) 
-    {      // we have just turned on
-      //Serial.println("Motion detected!");      // We only want to print on the output change, not state      
+    if (PIRState == LOW) // we have just turned on
+    { 
+      #ifdef SERIAL_TRACE
+        Serial.println("Mvt!");
+      #endif
       PIRState = HIGH;
-      //runShow();
-      state = LIGHTSHOW;
+      // get out of sleep/shutdown mode
+      setState(lastState);
     }  
   } 
   else 
   {    
-    if (PIRState == HIGH)
-    {      // we have just turned of
+    if (PIRState == HIGH) // we have just turned of
+    {
       //Serial.println("Motion ended!");      // We only want to print on the output change, not state
       PIRState = LOW;
       lastPIRTime = millis(); // we memorize the time of the end of the last movement
@@ -160,41 +281,52 @@ void movement()
 
 void sound()
 {
-  //Serial.println("Sound !");
-  //change light-show type
-  sndCounter++;
-  _break = true;
-  timerCallback();
+  // SND sensor can generate multiple successive interrupts, we allow only one each 500ms
+  unsigned long elapsedSinceLastSnd = millis() - lastSNDTime;
+  if(elapsedSinceLastSnd > SND_DELAY) 
+  {
+    //We had sound -> change show type
+    showType++;
+    //break out of loops
+    _break = true;
+    lastSNDTime = millis();
+    #ifdef SERIAL_TRACE
+        Serial.println("Snd!");
+    #endif
+  }
+  // a sound is treated as a movement to reset shutdown countdown
+  lastPIRTime = millis();
 }
 
 
 void timerCallback()
 {
   unsigned long elapsedSinceLastMvt = millis() - lastPIRTime;
-  //Serial.print("elapsed time: ");
-  //Serial.print(elapsedSinceLastMvt);
-  //Serial.println("ms");
   if(elapsedSinceLastMvt > NO_MVT_SHUTDOWN_DELAY)
   {
     if(state != SHUTDOWN)
     {
-      //Serial.println("Shuting Down"); 
-      state = SHUTDOWN;
+      #ifdef SERIAL_TRACE
+        Serial.println("Shuting Down ...");
+      #endif
+      setState(SHUTDOWN);
     }
   }
   else if(elapsedSinceLastMvt > NO_MVT_SLEEP_DELAY)
   {
     if(state != SLEEP)
     {
-      //Serial.println("Sleep Mode ZZzzZZ"); 
-      state = SLEEP;
+      #ifdef SERIAL_TRACE
+        Serial.println("Sleep Mode ZZzzZZ");
+      #endif
+      setState(SLEEP);
     }
   }
   else
   {
-    if(state != LIGHTSHOW)
+    if(state == SLEEP || state == SHUTDOWN)
     {
-      state = LIGHTSHOW;
+      setState(lastState);
     }
   }
 }
@@ -228,17 +360,20 @@ boolean colorFull(uint32_t c, unsigned long wait) {
       strip.setPixelColor(i, c);
   }
   strip.show();
-  unsigned long start = millis();
-  do
+  if(wait>0)
   {
-    delay(1);
-    if(_break)
+    unsigned long start = millis();
+    do
     {
-      _break = false;
-      return false;
+      delay(1);
+      if(_break)
+      {
+        _break = false;
+        return false;
+      }
     }
+    while(millis()-start<wait);
   }
-  while(millis()-start<wait);
   return true;
 }
 
